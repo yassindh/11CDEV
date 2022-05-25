8752052d1285652d4221900982a194a336a5b739
yassindh <yassin.damouh@student.uclouvain.be>
2022-05-25 18:01:52 +0200

    debut nav

--- main.c
+++ main.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+void userChoice()
+{
+    printf("Select a direction (N, S, E, W).\n Cancel the previous move(B).\n");
+    printf("Erase the chain (R).\n Restart the level (X).\n");
+    printf("Select another chain (C)\n");
+
+}
+
+
+int main()
+{
+    char direction;
+    char arr[5][5] = {{'x','1','1','1',' '},
+                     {' ',' ',' ','1',' '},
+                     {' ','1','1','1',' '},
+                     {' ','1',' ',' ','1'},
+                     {' ','1','1','1','1'}
+    };
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<5; j++) {
+            printf("%c ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    userChoice();
+    int positionx = 0;
+    int positiony = 0;
+    char East = 'e';
+    char West = 'w';
+    char North = 'n';
+    char South = 's';
+    for(int k=0; k<30; ++k){
+        printf(" une direction: \n");
+        scanf("%c", &direction);
+        if (direction == East && arr[positionx][positiony+1] == '1') {
+            positiony += 1;
+            arr[positionx][positiony] = '*';
+        }
+        else if(direction == South && arr[positionx+1][positiony] == '1'){
+            positionx += 1;
+            arr[positionx][positiony] = '*';
+        }
+        else if(direction == North && arr[positionx-1][positiony] == '1'){
+            positionx -= 1;
+            arr[positionx][positiony] = '*';
+        }
+        else if(direction == West && arr[positionx][positiony-1] == '1'){
+            positiony -= 1;
+            arr[positionx][positiony] = '*';
+        }
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<5; j++) {
+                printf("%c ", arr[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+
     return 0;
 }
